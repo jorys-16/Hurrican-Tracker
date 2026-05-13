c9423bf9e3cb75a1e25bf60e523196654ef33d91
jorys-16 <178527349+jorys-16@users.noreply.github.com>
2026-05-13 01:28:05 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,13 @@
+#include <QApplication>
+#include "mainwindow.h"
 
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    app.setApplicationName("Hurricane Tracker");
+    app.setApplicationVersion("1.0");
+
+    MainWindow window;
+    window.show();
+
+    return app.exec();
+}
